7c39303f25c6d7af2d381f0757ab70b937eeb79d
Martin <martin.kitukov@gmail.com>
2025-06-01 21:30:59 +0300

    refactoring structure

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from .user import User, UserRole
+from .developer import Developer, VerificationStatus
+from .project import Project, ProjectStatus, ProjectType
+from .subscription import Subscription, SubscriptionPlan
+from .saved_listing import SavedListing 
